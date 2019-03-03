322c761a91000a057217049387fd50da3d6cadde
Armand_laptop <517226@edu.rocmn.nl>
2019-03-03 16:37:18 +0100

    template structure added

---
+++ View/template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            include "inc/header.php";
+            include $viewMain;
+            include "inc/footer.php";
+        ?>
+    </body>
+</html>
